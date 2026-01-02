049c123b6c0186f8cccb44b22ba17f67f77a7389
innovoidlabs <innovoidlabs@gmail.com>
2026-01-02 20:21:35 +0530

    Fix static export build issues

--- next.config.js
+++ next.config.js
@@ -1,26 +1,24 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require('next-pwa');
-const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL || 'https://localhost:3000'}`);
 const nextConfig={
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: [`${hostname}`],
     unoptimized: true
   },
   output: 'export',
   trailingSlash: true,
-  distDir: 'out'
+  distDir: 'out',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  }
 }
-const pwa = process.env.NEXT_PWA_STATUS;
-const nextConfigWithPwa = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  disable: process.env.NODE_ENV === "development",
-}) (
-    nextConfig
-);
-module.exports = pwa==='1'?nextConfigWithPwa:nextConfig ;
+
+module.exports = nextConfig;
